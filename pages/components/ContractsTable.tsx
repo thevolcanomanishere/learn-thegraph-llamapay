ac32b7d173b5985597e9dc1e85de9c46a5c8a43a
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-29 15:11:37 +0100

    Fix component type and render

--- pages/components/ContractsTable.tsx
+++ pages/components/ContractsTable.tsx
@@ -57,7 +57,7 @@ const GET_CONTRACTS = gql`
   }
 `;
 
-const ContractsTable: () => false | JSX.Element = () => {
+const ContractsTable: FC = () => {
   const [result, reexecuteQuery] = useQuery({
     query: GET_CONTRACTS,
   });
@@ -141,8 +141,6 @@ const ContractsTable: () => false | JSX.Element = () => {
       //   };
       // })
       // .filter((contract: Contract) => contract.streams.length > 0);
-      const test = data.llamaPayFactories[0].contracts;
-      debugger;
       setContracts(filteredContracts);
       setStreams(filteredContracts.map((c: any) => c.streams));
       const tokens: ERC20BalanceCall[] = filteredContracts.map((token: any) => {
@@ -163,52 +161,52 @@ const ContractsTable: () => false | JSX.Element = () => {
     }
   }, [data, contracts, chainId]);
 
-  return (
-    mounted && (
-      <>
-        <Table striped={true}>
-          <Table.Head>
-            {headers.map((header, index) => (
-              <Table.HeadCell key={index}>{header}</Table.HeadCell>
-            ))}
-          </Table.Head>
-          <Table.Body className="divide-y">
-            {fetching ? (
-              <Spinner size="xl" />
-            ) : (
-              data &&
-              contracts &&
-              balances &&
-              contracts.map((contract, index) => (
-                <Table.Row key={index}>
-                  <Table.Cell
-                    onClick={() => openAddressInNewTab(contract.address)}
-                  >
-                    {shortenAddress(contract.address)}
-                  </Table.Cell>
-                  <Table.Cell className="flex">
-                    <div
-                      style={{
-                        backgroundColor: getBackgroundColor(
-                          contract.streams[0]?.payer.address
-                        ),
-                      }}
-                      className={`h-[20px] w-[20px] mr-2 rounded`}
-                    />
-                    {shortenAddress(contract.streams[0]?.payer.address)}
-                  </Table.Cell>
-                  <Table.Cell>{calculateActivePayees(contract)}</Table.Cell>
-                  <Table.Cell>{totalAmountPerSecond[index]}</Table.Cell>
-                  <Table.Cell>
-                    {balances[index]} {contract.token.symbol}
-                  </Table.Cell>
-                </Table.Row>
-              ))
-            )}
-          </Table.Body>
-        </Table>
-      </>
-    )
+  return !mounted ? (
+    <div>Loading...</div>
+  ) : (
+    <>
+      <Table striped={true}>
+        <Table.Head>
+          {headers.map((header, index) => (
+            <Table.HeadCell key={index}>{header}</Table.HeadCell>
+          ))}
+        </Table.Head>
+        <Table.Body className="divide-y">
+          {fetching ? (
+            <Spinner size="xl" />
+          ) : (
+            data &&
+            contracts &&
+            balances &&
+            contracts.map((contract, index) => (
+              <Table.Row key={index}>
+                <Table.Cell
+                  onClick={() => openAddressInNewTab(contract.address)}
+                >
+                  {shortenAddress(contract.address)}
+                </Table.Cell>
+                <Table.Cell className="flex">
+                  <div
+                    style={{
+                      backgroundColor: getBackgroundColor(
+                        contract.streams[0]?.payer.address
+                      ),
+                    }}
+                    className={`h-[20px] w-[20px] mr-2 rounded`}
+                  />
+                  {shortenAddress(contract.streams[0]?.payer.address)}
+                </Table.Cell>
+                <Table.Cell>{calculateActivePayees(contract)}</Table.Cell>
+                <Table.Cell>{totalAmountPerSecond[index]}</Table.Cell>
+                <Table.Cell>
+                  {balances[index]} {contract.token.symbol}
+                </Table.Cell>
+              </Table.Row>
+            ))
+          )}
+        </Table.Body>
+      </Table>
+    </>
   );
 };
 
