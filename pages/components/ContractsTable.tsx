99c710904f56d13da5f494de6a513d35dfe04de0
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-29 14:56:49 +0100

    Fix rendering errors in console

--- pages/components/ContractsTable.tsx
+++ pages/components/ContractsTable.tsx
@@ -1,12 +1,12 @@
 import { Spinner, Table } from "flowbite-react";
 import { useState, useContext } from "react";
 import { useQuery, gql } from "urql";
-import { Chain, chains } from "eth-chains";
 import { FC } from "react";
 import ChainContext, { IChainContext } from "../../utils/ChainContext";
 import { useEffect } from "react";
 import { ERC20BalanceCall, getERC20Balances } from "../../utils/chainCalls";
 import ColorHash from "color-hash";
+import { networks } from "../../utils/Networks";
 
 const colorHash = new ColorHash();
 
@@ -57,7 +57,7 @@ const GET_CONTRACTS = gql`
   }
 `;
 
-const ContractsTable: FC = () => {
+const ContractsTable: () => false | JSX.Element = () => {
   const [result, reexecuteQuery] = useQuery({
     query: GET_CONTRACTS,
   });
@@ -70,6 +70,11 @@ const ContractsTable: FC = () => {
   const [totalAmountPerSecond, setTotalAmountPerSecond] = useState<string[]>(
     []
   );
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const calculateActivePayees = (contract: Contract) => {
     return contract.streams.filter((stream) => stream.active && !stream.paused)
@@ -96,11 +101,10 @@ const ContractsTable: FC = () => {
   };
 
   const createExplorerLink = (address: string, chainId: number) => {
-    //TODO: check current chain ID and use correct explorer
-    const chain = chains.getById(chainId) as Chain;
-    if (chain && chain.explorers)
-      return chain.explorers[0].url + "address/" + address;
-    return "";
+    if (!address) return "";
+    return `${
+      networks.find((network) => network.chainId === chainId)?.explorer
+    }${address}`;
   };
 
   const createLinkToLlamaPay = (address: string, chainId: number = 1) => {
@@ -115,28 +119,44 @@ const ContractsTable: FC = () => {
     "Tokens Left",
   ];
 
+  const openAddressInNewTab = (address: string) => {
+    const url = createExplorerLink(address, chainId);
+    window.open(url, "_blank");
+  };
+
+  const getBackgroundColor = (address: string) => {
+    if (!address) return "black";
+    return colorHash.hex(address);
+  };
+
   useEffect(() => {
     if (data) {
-      setContracts(data.llamaPayFactories[0].contracts);
-      console.log(data);
-      setStreams(
-        data.llamaPayFactories[0].contracts.map((c: any) => c.streams)
-      );
-      const tokens: ERC20BalanceCall[] =
-        data.llamaPayFactories[0].contracts.map((token: any) => {
-          return {
-            address: token.address,
-            tokenAddress: token.token.address,
-            decimals: token.token.decimals,
-          };
-        });
+      const filteredContracts = data.llamaPayFactories[0].contracts;
+      // .map((contract: Contract) => {
+      //   return {
+      //     ...contract,
+      //     streams: contract.streams.filter(
+      //       (stream) => stream.active && !stream.paused
+      //     ),
+      //   };
+      // })
+      // .filter((contract: Contract) => contract.streams.length > 0);
+      const test = data.llamaPayFactories[0].contracts;
+      debugger;
+      setContracts(filteredContracts);
+      setStreams(filteredContracts.map((c: any) => c.streams));
+      const tokens: ERC20BalanceCall[] = filteredContracts.map((token: any) => {
+        return {
+          address: token.address,
+          tokenAddress: token.token.address,
+          decimals: token.token.decimals,
+        };
+      });
       const getBalances = async () => {
         const balances = await getERC20Balances(chainId, tokens);
         setBalances(balances);
       };
-      const totalAmountPerSecond = getTotalAmountPerSecond(
-        data.llamaPayFactories[0].contracts
-      );
+      const totalAmountPerSecond = getTotalAmountPerSecond(filteredContracts);
       setTotalAmountPerSecond(totalAmountPerSecond);
 
       getBalances();
@@ -144,47 +164,51 @@ const ContractsTable: FC = () => {
   }, [data, contracts, chainId]);
 
   return (
-    <Table striped={true}>
-      <Table.Head>
-        {headers.map((header, index) => (
-          <Table.HeadCell key={index}>{header}</Table.HeadCell>
-        ))}
-      </Table.Head>
-      <Table.Body className="divide-y">
-        {fetching ? (
-          <Spinner size="xl" />
-        ) : (
-          data &&
-          contracts &&
-          balances &&
-          contracts.map((contract, index) => (
-            <Table.Row key={index}>
-              <Table.Cell
-                className={`text-[${colorHash.hex(contract.address)}]`}
-              >
-                {shortenAddress(contract.address)}
-              </Table.Cell>
-              <Table.Cell className="flex">
-                <div
-                  style={{
-                    backgroundColor: colorHash.hex(
-                      contract.streams[0]?.payer.address
-                    ),
-                  }}
-                  className={`h-[20px] w-[20px] mr-2 rounded`}
-                />
-                {shortenAddress(contract.streams[0]?.payer.address)}
-              </Table.Cell>
-              <Table.Cell>{calculateActivePayees(contract)}</Table.Cell>
-              <Table.Cell>{totalAmountPerSecond[index]}</Table.Cell>
-              <Table.Cell>
-                {balances[index]} {contract.token.symbol}
-              </Table.Cell>
-            </Table.Row>
-          ))
-        )}
-      </Table.Body>
-    </Table>
+    mounted && (
+      <>
+        <Table striped={true}>
+          <Table.Head>
+            {headers.map((header, index) => (
+              <Table.HeadCell key={index}>{header}</Table.HeadCell>
+            ))}
+          </Table.Head>
+          <Table.Body className="divide-y">
+            {fetching ? (
+              <Spinner size="xl" />
+            ) : (
+              data &&
+              contracts &&
+              balances &&
+              contracts.map((contract, index) => (
+                <Table.Row key={index}>
+                  <Table.Cell
+                    onClick={() => openAddressInNewTab(contract.address)}
+                  >
+                    {shortenAddress(contract.address)}
+                  </Table.Cell>
+                  <Table.Cell className="flex">
+                    <div
+                      style={{
+                        backgroundColor: getBackgroundColor(
+                          contract.streams[0]?.payer.address
+                        ),
+                      }}
+                      className={`h-[20px] w-[20px] mr-2 rounded`}
+                    />
+                    {shortenAddress(contract.streams[0]?.payer.address)}
+                  </Table.Cell>
+                  <Table.Cell>{calculateActivePayees(contract)}</Table.Cell>
+                  <Table.Cell>{totalAmountPerSecond[index]}</Table.Cell>
+                  <Table.Cell>
+                    {balances[index]} {contract.token.symbol}
+                  </Table.Cell>
+                </Table.Row>
+              ))
+            )}
+          </Table.Body>
+        </Table>
+      </>
+    )
   );
 };
 
