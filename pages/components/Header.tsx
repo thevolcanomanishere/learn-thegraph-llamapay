35b58cff1c8c3f48dbd7ebc20770ea2f6168e82b
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-30 13:08:24 +0100

    Add more info

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex-row items-center h-24 mx-auto px-4">
-      <h1 className="w-auto pt-6 text-4xl font-bold text-black border-white border-b-2">
+    <div className="flex-row items-center h-32 mx-auto px-4">
+      <h1 className="w-auto my-6 text-4xl font-bold text-black border-white border-b-2">
         🦙 LlamaPay Contracts
       </h1>
+      <p>
+        Click on the contract addresses to open the explorer for that chain.
+      </p>
+      <p>Click on the payer address to open LlamaPay.io for that address.</p>
     </div>
   );
 };
